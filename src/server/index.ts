a32898dd4f9ca50e045019ae42854f7760522aa3
Andre Ferraro <aferrarobr@gmail.com>
2020-05-08 13:50:46 +0200

    Fastify

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,39 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+import Next from 'next'
+const fastify = require('fastify')({ logger: { level: 'error' } })
 
-const port = parseInt(process.env.PORT || '3000', 10)
+console.log(process.env.PORT)
+
+const port = parseInt(process.env.PORT || '3000', 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+fastify.register((fastify: any, _opts: any, next: any) => {
+  const app = Next({ dev })
+  const handle = app.getRequestHandler()
+  app
+    .prepare()
+    .then(() => {
+      if (dev) {
+        fastify.get('/_next/*', async (req: { req: import("http").IncomingMessage }, reply: { res: import("http").ServerResponse; sent: boolean }) => {
+          await handle(req.req, reply.res)
+          reply.sent = true
+        })
+      }
+      fastify.all('/*', async (req: { req: import("http").IncomingMessage }, reply: { res: import("http").ServerResponse; sent: boolean }) => {
+        await handle(req.req, reply.res)
+        reply.sent = true
+      })
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
+      fastify.setNotFoundHandler(async (request: { req: import("http").IncomingMessage }, reply: { res: import("http").ServerResponse; sent: boolean }) => {
+        await app.render404(request.req, reply.res)
+        reply.sent = true
+      })
+
+      next()
+    })
+    .catch(err => next(err))
+})
+
+fastify.listen(port, (err: any) => {
+  if (err) throw err
+  console.log(`> Ready on http://localhost:${port}`)
 })
